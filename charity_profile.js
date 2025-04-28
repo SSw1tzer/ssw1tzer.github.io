785096d4b1210365e59ad44a532fd99639d962ec
Dusk Walker <sswitzer064@gmail.com>
2025-04-27 21:15:34 -0400

    added theme transition button on home page

--- charity_profile.js
+++ charity_profile.js
@@ -1,3 +1,15 @@
 function disForm () {
     document.getElementById("volunteerForm").style.display = "block";
 }
+
+function toggleTheme() {
+    // Get the current theme from the document's root element
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+  
+    // Toggle between light and dark
+    if (currentTheme === 'light') {
+      document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+    }
+  }
